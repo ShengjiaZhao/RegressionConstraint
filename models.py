da92d69727be1ad983d624b993e24b85cbe2eabd
ShengjiaZhao <sjzhao@stanford.edu>
2021-01-19 16:32:52 -0800

    Add padding

--- models.py
+++ models.py
@@ -112,9 +112,11 @@ def eval_bias_knn(model, data, args, axis='label'):
     sorted_outputs = outputs[ranking]
 
     smoothed_outputs = F.conv1d(sorted_outputs.view(1, 1, -1), 
-                                weight=(1./ (k+1) * torch.ones(1, 1, k+1, device=args.device, requires_grad=False))).flatten()
+                                weight=(1./ (k+1) * torch.ones(1, 1, k+1, device=args.device, requires_grad=False)),
+                                padding=k // 2).flatten()
     smoothed_labels = F.conv1d(sorted_labels.view(1, 1, -1), 
-                               weight=(1./ (k+1) * torch.ones(1, 1, k+1, device=args.device, requires_grad=False))).flatten()
+                               weight=(1./ (k+1) * torch.ones(1, 1, k+1, device=args.device, requires_grad=False)),
+                               padding=k // 2).flatten()
     loss_bias = smoothed_labels - smoothed_outputs
     return loss_bias.pow(2).mean(), loss_bias
 
