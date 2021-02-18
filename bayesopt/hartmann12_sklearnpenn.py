e223043d2b73c41b1183c7adcc6b54153de27e0d
willieneis <willie.neiswanger@gmail.com>
2021-02-17 18:22:07 -0800

    bayesopt: add hartmann functions and script for BO on hartmann12

--- bayesopt/hartmann12_sklearnpenn.py
+++ bayesopt/hartmann12_sklearnpenn.py
@@ -2,13 +2,13 @@ from argparse import Namespace
 import numpy as np
 
 from probo import NelderMeadAcqOptimizer, SimpleBo
-from branin import branin, get_branin_domain_nd
+from hartmann import hartmann_nd, get_hartmann_domain_nd
 from penn_sklearn import SklearnPenn
 
 
 # define function
-ndimx = 40
-f = lambda x: np.sum([branin(x[2 * i : 2 * i + 2]) for i in range(ndimx // 2)])
+n_dim = 12
+f = hartmann_nd
 
 # define model
 model = SklearnPenn()
@@ -17,19 +17,19 @@ model = SklearnPenn()
 acqfunction = {'acq_str': 'ts', 'n_gen': 500}
 
 # define acqoptimizer
-domain = get_branin_domain_nd(ndimx)
+domain = get_hartmann_domain_nd(n_dim)
 acqoptimizer = NelderMeadAcqOptimizer(
     {'rand_every': 10, 'max_iter': 200, 'jitter': True}, domain
 )
 
 # define  initial dataset
-n_init = 80
+n_init = 100
 data = Namespace()
 data.x = domain.unif_rand_sample(n_init)
 data.y = [f(x) for x in data.x]
 
 # define and run BO
 bo = SimpleBo(
-    f, model, acqfunction, acqoptimizer, data=data, params={'n_iter': 120}, seed=11
+    f, model, acqfunction, acqoptimizer, data=data, params={'n_iter': 200}, seed=11
 )
 results = bo.run()
